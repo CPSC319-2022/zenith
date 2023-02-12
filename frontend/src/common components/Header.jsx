a5f7ee060ac1a2337c1ed13bf057f326e65c325a
Anusha <anushasaleem.dev@gmail.com>
2023-02-12 00:10:38 -0800

    header + footer completed

---
+++ frontend/src/common components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Container, Nav, Navbar} from "react-bootstrap";
+import blogImg from '../images/blog.svg'
+import '../styles/Header.css';
+
+const Header = () => {
+    return (
+        <Navbar className="navbar">
+            <Container className="header-container">
+                <Navbar.Brand className="blog-img" href="/">
+                    <img
+                        src={blogImg}
+                        width="120"
+                        height="70"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Nav className="ml-auto" placement="end">
+                    <Nav.Link href="/">HOME</Nav.Link>
+                    <Nav.Link href="/create">CREATE</Nav.Link>
+                    <Nav.Link href="/profile">PROFILE</Nav.Link>
+                    <Nav.Link href="/login">LOGOUT</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
