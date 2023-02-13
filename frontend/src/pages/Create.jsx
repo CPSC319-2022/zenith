2326a4fdb36e1cf159830dab64317e67d51518f9
Anusha <anushasaleem.dev@gmail.com>
2023-02-12 17:06:53 -0800

    icon + create post done

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import '../styles/Create.css';
 
 const Create = () => {
+    const [value, setValue] = useState('');
+
     return (
-        <div>
-            Write
+        <div className="create">
+            <div className="content">
+                <input type="text" name="title" placeholder="Title" />
+                <div className="editor-container">
+                    <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />
+                </div>
+            </div>
+            <div className="menu">
+                <div className="menu-item">
+                    <h1 className="publish">Publish</h1>
+                    <span className="visibility">
+                        <b> Visibility:</b> Public
+                    </span>
+                    <br/>
+                    <input style={{display: 'none'}} type="file" id="file" />
+                    <label className="img-upload" htmlFor="file">Upload Image</label>
+                    <div className="buttons">
+                        <button className="btn1">Save Draft</button>
+                        <button className="btn2">Update</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
