95552e781f504963da214d8f1f7ebb6a3503ab2e
erickswong <erickswong@hotmail.com>
2023-02-26 01:13:41 -0800

    Update Post

--- blog/src/main/java/com/blog/model/Post.java
+++ blog/src/main/java/com/blog/model/Post.java
@@ -1,28 +1,87 @@
 package com.blog.model;
 
 import java.time.Clock;
+import java.util.Objects;
 
 public class Post {
-
-    private Integer post_ID;
-
-    private Integer author_ID;
-
-    private Integer views;
-
+    private final int postID;
+    private final int authorID;
     private String title;
-
     private String content;
-
-    private Clock creationDate;
-
+    private final Clock creationDate;
     private Clock lastModified;
+    private int views;
+    private boolean allowComments;
+    // private Tag tags; TODO: allow tags for posts
+    // private int upvotes; TODO: allow upvoting/downvoting?
 
-    private Boolean allowComment;
+    public Post(int postID,
+                int authorID,
+                String title,
+                String content,
+                Clock creationDate,
+                Clock lastModified,
+                int views,
+                boolean allowComments) {
+        this.postID = postID;
+        this.authorID = authorID;
+        this.title = title;
+        this.content = content;
+        this.creationDate = creationDate;
+        this.lastModified = lastModified;
+        this.views = views;
+        this.allowComments = allowComments;
+    }
 
-    private Integer commentsNum;
+    public int getPostID() {
+        return postID;
+    }
 
-    //todo
-    //private Tag tags;
+    public int getAuthorID() {
+        return authorID;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Clock getCreationDate() {
+        return creationDate;
+    }
+
+    public Clock getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Clock lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public boolean isAllowComments() {
+        return allowComments;
+    }
+
+    public void setAllowComment(boolean allowComments) {
+        this.allowComments = allowComments;
+    }
 }
