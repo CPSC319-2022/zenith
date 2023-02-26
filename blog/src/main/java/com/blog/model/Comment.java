7484d906faed66f351e580fe5c884c0be700d58b
erickswong <erickswong@hotmail.com>
2023-02-26 01:13:50 -0800

    Update Comment

--- blog/src/main/java/com/blog/model/Comment.java
+++ blog/src/main/java/com/blog/model/Comment.java
@@ -3,15 +3,57 @@ package com.blog.model;
 import java.time.Clock;
 
 public class Comment {
-    private Integer commentID;
+    private final int commentID;
+    private final int authorID;
+    private final int postID;
+    private String content;
+    private final Clock creationDate;
+    private Clock lastModified;
+    // private int upvotes; TODO: allow upvoting/downvoting?
 
-    private Integer authorID;
+    public Comment(int commentID,
+                   int authorID,
+                   int postID,
+                   String content,
+                   Clock creationDate,
+                   Clock lastModified) {
+        this.commentID = commentID;
+        this.authorID = authorID;
+        this.postID = postID;
+        this.content = content;
+        this.creationDate = creationDate;
+        this.lastModified = lastModified;
+    }
 
-    private String title;
+    public int getCommentID() {
+        return commentID;
+    }
 
-    private Clock creationDate;
+    public int getAuthorID() {
+        return authorID;
+    }
 
-    private boolean isParent;
+    public int getPostID() {
+        return postID;
+    }
 
-    private Integer parentCommentID;
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Clock getCreationDate() {
+        return creationDate;
+    }
+
+    public Clock getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Clock lastModified) {
+        this.lastModified = lastModified;
+    }
 }
