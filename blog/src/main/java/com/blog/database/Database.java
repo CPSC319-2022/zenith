9f66906aed15a378bd89722b5d611ddd72748705
erickswong <erickswong@hotmail.com>
2023-03-04 16:22:27 -0800

    Update Database stubs

--- blog/src/main/java/com/blog/database/Database.java
+++ blog/src/main/java/com/blog/database/Database.java
@@ -9,6 +9,11 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  * This class handles all calls to the database related to the blog application.
  */
 public class Database {
+    /*
+     * TODO: Need some static fields to store the connection to the database.
+     *       Need static method to make the connection (maybe also close)
+     */
+
     /**
      * TODO
      *
@@ -19,6 +24,9 @@ public class Database {
         int commentID = comment.getCommentID();
 
         throw new NotImplementedException();
+        /*
+        TODO: if comment does not exist, return without error. but make sure that you do comment.setContent(null) first.
+         */
     }
 
     /**
@@ -30,6 +38,9 @@ public class Database {
         int postID = post.getPostID();
 
         throw new NotImplementedException();
+        /*
+        TODO: if post does not exist, return without error. but make sure that you do post.setContent(null) first.
+         */
     }
 
     /**
@@ -43,12 +54,102 @@ public class Database {
 
         throw new NotImplementedException();
         /*
-        TODO: @Cheryl
-              Use userID to select corresponding user record from database.
+        TODO: Use userID to select corresponding user record from database.
               Then use setter functions from User to fill out the private fields
+              Maybe throw an exception if user does not exist, e.g. throw new UserDoesNotExistException()
+              If user is deleted maybe throw new UserIsDeletedException()
          */
     }
 
-    // TODO: corresponding static methods for updating a record
-    //       maybe a range retrieve? e.g. retrieve next k posts starting from n
+    /**
+     * TODO
+     *
+     * @param comment
+     */
+    public static void save(Comment comment) {
+        int postID = comment.getPostID();
+        int commentID = comment.getCommentID();
+
+        throw new NotImplementedException();
+        /*
+        TODO: if keys already exist in database, update
+              otherwise, create new record with the keys
+         */
+    }
+
+    /**
+     * TODO
+     *
+     * @param post
+     */
+    public static void save(Post post) {
+        int postID = post.getPostID();
+
+        throw new NotImplementedException();
+        /*
+        TODO: if keys already exist in database, update
+              otherwise, create new record with the keys
+         */
+    }
+
+    /**
+     * Saves the given <code>User</code> object into the database.
+     *
+     * @param user  The <code>User</code> object to save. Contains the <code>userID</code>.
+     */
+    public static void save(User user) {
+        int userID = user.getUserID();
+
+        throw new NotImplementedException();
+        /*
+        TODO: if keys already exist in database, update
+              otherwise, create new record with the keys
+         */
+    }
+
+    /**
+     * TODO
+     *
+     * @param comment
+     */
+    public static void delete(Comment comment) {
+        int postID = comment.getPostID();
+        int commentID = comment.getCommentID();
+
+        throw new NotImplementedException();
+        /*
+        TODO: don't hard delete the record
+              instead, we'll soft delete by setting a deleted flag
+         */
+    }
+
+    /**
+     * TODO
+     *
+     * @param post
+     */
+    public static void delete(Post post) {
+        int postID = post.getPostID();
+
+        throw new NotImplementedException();
+        /*
+        TODO: don't hard delete the record
+              instead, we'll soft delete by setting a deleted flag
+         */
+    }
+
+    /**
+     * Saves the given <code>User</code> object into the database.
+     *
+     * @param user  The <code>User</code> object to save. Contains the <code>userID</code>.
+     */
+    public static void delete(User user) {
+        int userID = user.getUserID();
+
+        throw new NotImplementedException();
+        /*
+        TODO: don't hard delete the record
+              instead, we'll soft delete by setting a deleted flag
+         */
+    }
 }
