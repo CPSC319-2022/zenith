bfb8422db4b2fa030dbe748b3260ea3025437641
Shawn_hash <sherlock01_zhu@outlook.com>
2023-03-10 14:48:48 -0800

    try-one with few tests

--- blog/src/test/java/com/blog/model/UserTest.java
+++ blog/src/test/java/com/blog/model/UserTest.java
@@ -1,15 +1,33 @@
 package com.blog.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.blog.model.UserLevel.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
+    private User guest0;
+
+    @BeforeEach
+    void beforeEach() {
+        guest0 = new User();
+    }
+
     @Test
     void guestConstructor() {
-        User guest = new User();
+        assertEquals(0, guest0.getUserID());
+        assertEquals(UserLevel.GUEST, guest0.getUserLevel());
+    }
 
-        assertEquals(0, guest.getUserID());
-        assertEquals(UserLevel.GUEST, guest.getUserLevel());
+    @Test
+    void guestToOtherRoles() {
+        assertEquals(UserLevel.GUEST, guest0.getUserLevel());
+        guest0.setUserLevel(READER);
+        assertEquals(UserLevel.GUEST, guest0.getUserLevel());
+        guest0.setUserLevel(CONTRIBUTOR);
+        assertEquals(UserLevel.CONTRIBUTOR, guest0.getUserLevel());
+        guest0.setUserLevel(ADMIN);
+        assertEquals(UserLevel.ADMIN, guest0.getUserLevel());
     }
 }
