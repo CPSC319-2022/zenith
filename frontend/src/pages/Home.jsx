13643d98a7c077259e0084e1012286bbb2c42b36
Anusha <anushasaleem.dev@gmail.com>
2023-03-10 13:54:25 -0800

    introduced pagination of 4 posts per page into blog. will connect with DB posts when db is connected

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {fakePosts} from '../data/fakePosts';
 import {Link} from "react-router-dom";
 import {Button} from "react-bootstrap";
 import "../styles/Home.css"
+import Pagination from "./Pagination";
 
 const Home = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(4);
+
+    const lastPostIndex = currentPage * postsPerPage;
+    const firstPostIndex = lastPostIndex - postsPerPage;
+    const currentPosts = fakePosts.slice(firstPostIndex, lastPostIndex);
+
     const navigate = useNavigate();
     function handleClick(id) {
         navigate(`/single-post/${id}`);
@@ -14,7 +22,7 @@ const Home = () => {
     return (
         <div className="home">
             <div className="posts">
-                {fakePosts.map(post => (
+                {currentPosts.map(post => (
                     <div className="post" key={post.id}>
                         <div className="img">
                             <img src={post.img} alt=""/>
@@ -31,6 +39,13 @@ const Home = () => {
                     </div>
                 ))}
             </div>
+
+            <Pagination
+                totalPosts={fakePosts.length}
+                postsPerPage={postsPerPage}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+            />
         </div>
     );
 };
