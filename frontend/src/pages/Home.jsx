276e6e09185db85ae8737dfc65e103b967173387
Anusha <anushasaleem.dev@gmail.com>
2023-03-10 12:48:21 -0800

    'Read More' button on home page navigates to respective post

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import {fakePosts} from '../data/fakePosts';
 import {Link} from "react-router-dom";
 import {Button} from "react-bootstrap";
 import "../styles/Home.css"
 
 const Home = () => {
+    const navigate = useNavigate();
+    function handleClick(id) {
+        navigate(`/single-post/${id}`);
+    }
+
     return (
         <div className="home">
             <div className="posts">
@@ -19,7 +25,8 @@ const Home = () => {
                                 <h1 className="post-title">{post.title}</h1>
                             </Link>
                             <p className="post-content">{post.content}</p>
-                            <Button className="read-button">Read More</Button>
+                            <Button className="read-button"
+                                    onClick={() => handleClick(`${post.id}`)}>Read More</Button>
                         </div>
                     </div>
                 ))}
