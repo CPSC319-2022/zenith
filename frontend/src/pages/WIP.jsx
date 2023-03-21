021c3e381b7e820957e3f29c8928c60bbca89fb9
saadishahid <39744515+saadishahid@users.noreply.github.com>
2023-03-20 18:33:02 -0700

    updated WIP link

--- frontend/src/pages/WIP.jsx
+++ frontend/src/pages/WIP.jsx
@@ -5,12 +5,12 @@ const WorkInProgress = () => {
         <div>
             <h1>WIP</h1>
             <p>Initial version of  frontend React JS  app has been developed.</p>
-<p>Frontend app has been deployed with Google Cloud</p>
-<p>Docker container has been setup and used for deployment</p>
-<p>Nginx configurations for serving the app has been completed</p>
-<p>Basic CI/CD pipeline for building and deploying the app for prod environment has been made using Google Cloud Build and Google Cloud Run. </p>
-<p>Backend API work is in progress. 70% completed</p>
-<p>Database setup completed.</p>
+<p>Basic Blog app has been deployed with Google Cloud</p>
+<p>Both Frontend and Backend docker images are running on GCP</p>
+<p>Work in progres on CI/CD for multiple dev environments</p>
+<p>Work in progress for Slackbot</p>
+<p>Work in progress for Unit testing. 79% coverage for Model package.</p>
+
 
         </div>
     );
