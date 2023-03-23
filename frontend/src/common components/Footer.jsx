337220dbc59a094c14fd72b51adc6725ed42c4c3
Anusha <anushasaleem.dev@gmail.com>
2023-03-22 20:01:57 -0700

    slacktest 3

--- frontend/src/common components/Footer.jsx
+++ frontend/src/common components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <span>
-                Team Zenith for CPSC 319 Anusha
+                Team Zenith for CPSC 319
             </span>
         </footer>
     );
