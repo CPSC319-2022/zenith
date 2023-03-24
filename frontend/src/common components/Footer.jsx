e1b4fb758c3d7a58c0a9de7a002787a36ebe6453
Anusha <anushasaleem.dev@gmail.com>
2023-03-23 20:56:31 -0700

    changed cloud function

--- frontend/src/common components/Footer.jsx
+++ frontend/src/common components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <span>
-                Team Zenith for CPSC 319 anusha bts
+                Team Zenith
             </span>
         </footer>
     );
