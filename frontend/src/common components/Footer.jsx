a4b32dc0cbee5d2b02c7a6f147269430708efc09
Anusha <anushasaleem.dev@gmail.com>
2023-03-23 20:32:27 -0700

    added _BUILD_STATUS as substitution variable madhv

--- frontend/src/common components/Footer.jsx
+++ frontend/src/common components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <span>
-                Team Zenith for CPSC 319 anusha
+                Team Zenith for CPSC 319
             </span>
         </footer>
     );
