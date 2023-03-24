d4002ffdd928d7ddf71ae6ff7d5ce063da8b42b4
Anusha <anushasaleem.dev@gmail.com>
2023-03-23 21:09:34 -0700

    chnged cloud function

--- frontend/src/common components/Footer.jsx
+++ frontend/src/common components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <span>
-                Team Zenith
+                Team Zenith blaa
             </span>
         </footer>
     );
