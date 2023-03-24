2d2c4ad9df28ce9c639305bfc16044624f909264
Anusha <anushasaleem.dev@gmail.com>
2023-03-23 23:10:50 -0700

    min

--- frontend/src/common components/Footer.jsx
+++ frontend/src/common components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <span>
-                Team Zenith CPSC 319dewdwedwgere
+                Team Zenith CPSC 319dewdwedwgere;k;k
             </span>
         </footer>
     );
