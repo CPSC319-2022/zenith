f23c5a21e1e875a16328800c0a3af771845c6de6
Anusha <anushasaleem.dev@gmail.com>
2023-03-26 14:58:11 -0700

    added slack steps to cloudbuild file

--- frontend/src/common components/Footer.jsx
+++ frontend/src/common components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <span>
-                Team Zenith CPSC 319dewdwedwgere;k;k
+                Team Zenith CPSC 319 trying slack
             </span>
         </footer>
     );
