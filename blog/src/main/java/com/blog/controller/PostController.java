4f7adc0817f5821a4d674e77136dd9ba328d3732
erickswong <erickswong@hotmail.com>
2023-03-29 01:38:50 -0700

    Remove unnecessary import

--- blog/src/main/java/com/blog/controller/PostController.java
+++ blog/src/main/java/com/blog/controller/PostController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.blog.model.Post.NEW_POST_ID;
