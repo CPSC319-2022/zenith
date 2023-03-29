e34cb65c841e4828780857f41c4ede301bafd798
erickswong <erickswong@hotmail.com>
2023-03-28 17:46:14 -0700

    Add error checking for uploading image

--- blog/src/main/java/com/blog/storage/GoogleCloudStorage.java
+++ blog/src/main/java/com/blog/storage/GoogleCloudStorage.java
@@ -1,5 +1,6 @@
 package com.blog.storage;
 
+import com.blog.exception.BlogException;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
@@ -11,18 +12,26 @@ import java.util.UUID;
 
 @Service
 public class GoogleCloudStorage {
-    public static final String DEFAULT_IMAGE_URL = ""; // TODO
+    public static final String DEFAULT_IMAGE_URL = ""; // TODO: add default url
 
     private static final String BUCKET_NAME = "zenith-blog-storage";
     private static final Storage STORAGE = GoogleCloudStorageConfig.storage();
 
-    public static String uploadImage(MultipartFile image) throws IOException {
+    public static String uploadImage(MultipartFile image) throws BlogException {
+        if (image == null) {
+            return DEFAULT_IMAGE_URL;
+        }
+
         String imageName = UUID.randomUUID().toString();
         String imageExtension = getImageExtension(image.getOriginalFilename());
         String blobName = imageName + "." + imageExtension;
 
         BlobInfo blobInfo = BlobInfo.newBuilder(BUCKET_NAME, blobName).build();
-        Blob blob = STORAGE.create(blobInfo, image.getBytes());
+        try {
+            STORAGE.create(blobInfo, image.getBytes());
+        } catch (IOException e) {
+            throw new BlogException("Failed to upload image to Google Cloud Storage.");
+        }
 
         // If your bucket is public, you can use this URL format
         return String.format("https://storage.googleapis.com/%s/%s", BUCKET_NAME, blobName);
