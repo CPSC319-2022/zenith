c086842707222047a63eb6ded2c96ff112a5047a
saadishahid <39744515+saadishahid@users.noreply.github.com>
2023-04-01 17:37:21 -0700

    conditional renedering of admin

--- frontend/src/pages/WIP.jsx
+++ frontend/src/pages/WIP.jsx
@@ -5,11 +5,10 @@ const WorkInProgress = () => {
     return (
         <div>
             <h1>WIP</h1>
-            <p>Initial version of  frontend React JS  app has been developed.</p>
-            <p>Basic Blog app has been deployed with Google Cloud</p>
-            <p>Both Frontend and Backend docker images are running on GCP</p>
-            <p>Work in progres on CI/CD for multiple dev environments</p>
-            <p>Work in progress for Slackbot</p>
+            <p>Blog application has been developed</p>
+            <p>CI/CD pipeline with Google Build is operational and supports multiple dev environments</p>
+            <p> Slackbot is 80% complete</p>
+ 
             <p>Work in progress for Unit testing. 79% coverage for Model package.</p>
         </div>
     );
