8721869c9ff4c8a641996e6b1ab1c7c155acd108
baekanthony <53468745+baekanthony@users.noreply.github.com>
2023-04-03 15:29:29 -0700

    testing changes

--- frontend/src/pages/WIP.jsx
+++ frontend/src/pages/WIP.jsx
@@ -5,9 +5,9 @@ const WorkInProgress = () => {
     return (
         <div>
             <h1>WIP</h1>
-            <p>Blog application has been developed</p>
-            <p>CI/CD pipeline with Google Build is operational and supports multiple dev environments</p>
-            <p> Slackbot is 80% complete</p>
+            <p>Blog application has been developed.</p>
+            <p>CI/CD pipeline with Google Build is operational and supports multiple dev environments.</p>
+            <p> Slackbot is 80% complete.</p>
  
             <p>Work in progress for Unit testing. 79% coverage for Model package.</p>
         </div>
