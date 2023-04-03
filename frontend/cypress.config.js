335185aa79d27f767535ef5bde8ac03ddfd163bd
Anusha <anushasaleem.dev@gmail.com>
2023-04-02 20:37:17 -0700

    more test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,10 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 800,
+
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
